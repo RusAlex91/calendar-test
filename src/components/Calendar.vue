bf75f763b17cf673bd1a87f77ddf17ffedcc846d
RusAlex91 <aocrusalex91@yandex.ru>
2021-10-13 00:53:31 +0300

    minor revork calendar days data

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="calendar">
+  <div class="calendar" @click="todayDate()">
     <div class="calendar__navigation">
       <h2 ref="date" class="calendar__month-year">Июнь 2021</h2>
-      {{ this.holidays }}
       <div class="calendar-controls">
         <button
           @click="updateCalendar('descrease')"
@@ -34,20 +33,19 @@
           v-for="index in prevDates"
           :key="index"
           :index="index"
+          :prev-date="true"
         ></calendar-day>
         <calendar-day
-          v-for="index in daysInMonth"
+          v-for="(value, key, index) in day"
           :key="index"
-          :index="index"
-          :day="this.currentDay"
+          :dayInfo="value"
           :weekend="this.weekends"
-          :holiday="this.holidays"
-          :trip="this.trips"
         ></calendar-day>
         <calendar-day
           v-for="index in nextDates"
           :key="index"
           :index="index"
+          :prev-date="true"
         ></calendar-day>
       </div>
     </div>
@@ -60,6 +58,33 @@ export default {
   components: {
     CalendarDay
   },
+  props: {
+    holidaysData: {
+      type: Array,
+      required: false,
+      default: () => [
+        '1.1.2021',
+        '2.1.2021',
+        '3.1.2021',
+        '4.1.2021',
+        '5.1.2021',
+        '6.1.2021',
+        '7.1.2021',
+        '8.1.2021',
+        '23.2.2021',
+        '8.3.2021',
+        '1.5.2021',
+        '9.5.2021',
+        '12.6.2021',
+        '4.11.2021'
+      ]
+    },
+    tripsData: {
+      type: Object,
+      required: false,
+      default: Object
+    }
+  },
   data () {
     return {
       date: new Date(),
@@ -69,21 +94,41 @@ export default {
       currentDay: null,
       weekends: [],
       holidays: [],
-      trips: []
+      trips: [],
+      tripData: {},
+      holidayData: [],
+      tripArr: [],
+      day: {}
     }
   },
   methods: {
     updateCalendar (value) {
       if (value === 'descrease') {
         this.date.setMonth(this.date.getMonth() - 1)
+        this.daysInMonth = null
+        this.prevDates = []
+        this.nextDates = []
+        this.currentDay = null
         this.weekends = []
-        this.holidays = []
+
         this.trips = []
+        this.tripData = {}
+
+        this.tripArr = []
+        this.day = {}
       } else if (value === 'increase') {
         this.date.setMonth(this.date.getMonth() + 1)
+        this.daysInMonth = null
+        this.prevDates = []
+        this.nextDates = []
+        this.currentDay = null
         this.weekends = []
-        this.holidays = []
+
         this.trips = []
+        this.tripData = {}
+
+        this.tripArr = []
+        this.day = {}
       }
       this.createCalendar()
     },
@@ -131,11 +176,18 @@ export default {
 
       this.prevDates = []
       const firstDayIndex = date.getDay() - 1
+      // const firstDaysObj = {}
 
       for (let x = firstDayIndex; x > 0; x--) {
+        // firstDaysObj[x] = {}
+
         const temp = prevLastDay - x + 1
+
+        // firstDaysObj[x].day = temp
+
         this.prevDates.push(temp)
       }
+      // this.prevDates = firstDaysObj
 
       this.nextDates = []
       const nextDays = 7 - lastDayIndex
@@ -147,77 +199,71 @@ export default {
 
       const month = this.date.getMonth() + 1
       var current = new Date(this.date.getFullYear(), month - 1, 1)
-
+      const objDay = {}
       // as long as our date is in the requested month
+
+      const daysArr = [...Array(this.daysInMonth).keys()]
+
+      for (let currentDay = 0; currentDay < daysArr.length; currentDay++) {
+        let today = this.date
+        const dd = daysArr[currentDay]
+        const mm = month
+        const yyyy = today.getFullYear()
+
+        today = dd + '.' + mm + '.' + yyyy
+
+        objDay[currentDay] = {}
+        objDay[currentDay].day = currentDay + 1
+        objDay[currentDay].weekend = false
+
+        this.holidayData.forEach(el => {
+          if (today === el) {
+            this.holidays.push(dd)
+
+            objDay[dd - 1].holidays = true
+          }
+        })
+
+        Object.entries(this.tripData).forEach(([key, value]) => {
+          value.forEach(el => {
+            if (today === el) {
+              this.trips.push(dd)
+              objDay[dd - 1].trip = true
+              objDay[dd - 1].location = key
+            }
+          })
+        })
+      }
+      let dd = 0
       while (current.getMonth() === month - 1) {
         // saturday or sunday?
+
         if (current.getDay() === 0 || current.getDay() === 6) {
           this.weekends.push(current.getDate())
+          objDay[dd].weekend = true
         }
 
         // move to next day
         current.setDate(current.getDate() + 1)
+        dd++
       }
-
-      const daysArr = [...Array(this.daysInMonth).keys()]
-      for (let currentDay = 0; currentDay < daysArr.length; currentDay++) {
-        let today = this.date
-        const dd = daysArr[currentDay]
-        const mm = month
-        const yyyy = today.getFullYear()
-
-        today = dd + '.' + mm + '.' + yyyy
-
-        const arr = [
-          '11.10.2021',
-          '2.1.2021',
-          '3.1.2021',
-          '4.1.2021',
-          '5.1.2021',
-          '6.1.2021',
-          '7.1.2021',
-          '8.1.2021',
-          '23.2.2021',
-          '8.3.2021',
-          '1.5.2021',
-          '9.5.2021',
-          '12.6.2021',
-          '4.11.2021'
-        ]
-        arr.forEach(el => {
-          if (today === el) {
-            this.holidays.push(dd)
-          }
-        })
-      }
-
-      for (let currentDay = 0; currentDay < daysArr.length; currentDay++) {
-        let today = this.date
-        const dd = daysArr[currentDay]
-        const mm = month
-        const yyyy = today.getFullYear()
-
-        today = dd + '.' + mm + '.' + yyyy
-
-        const arr = [
-          '14.10.2021',
-          '15.10.2021',
-          '16.10.2021',
-          '17.10.2021',
-          '18.10.2021'
-        ]
-        arr.forEach(el => {
-          if (today === el) {
-            this.trips.push(dd)
-          }
-        })
-      }
+      this.day = objDay
     }
   },
+
   mounted () {
     this.createCalendar()
   },
-  afterMounted () {}
+  updated () {
+    this.holidayData = this.holidaysData
+    this.tripData = this.tripsData
+  },
+  watch: {
+    tripsData: function () {
+      this.tripData = this.tripsData
+      this.createCalendar()
+    }
+  }
 }
 </script>
 
